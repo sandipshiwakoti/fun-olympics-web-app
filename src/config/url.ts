872c792423f17a407e8dcede9aa583e7bcdff93c
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2023-01-01 23:47:10 +0545

    fix: add image fallback url

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,4 +1,5 @@
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+export const imageFallbackUrl = "https://mapbiomas.org/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png";
 
 export const urls = {
   login: "/auth/login",
